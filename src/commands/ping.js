217ec61eee9ecfceecf34eb575528600e206cd74
team-universal <devinsane.79@gmail.com>
2022-09-26 14:56:16 +0530

    Universal: modified 2 commands

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -11,7 +11,7 @@ module.exports = {
        */
       run: async (universal, interaction) => {
             await interaction.reply({
-                  content: `\` My current latency is: ${universal.ws.ping} \``
+                  content: `\` My current latency is: ${universal.ws.ping}ms \``
             });
       }
 };
