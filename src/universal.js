126b81aec420536cfe297f404f8a9f0b108bc461
team-universal <devinsane.79@gmail.com>
2022-09-28 16:17:58 +0530

    Universal: modified UI

--- src/universal.js
+++ src/universal.js
@@ -1,8 +1,15 @@
+const { join } = require('node:path');
 const { UniClient } = require('./classes/Client');
 const { settings } = require('./config/config');
+const wait = require('node:timers/promises').setTimeout;
 
 const Universal = new UniClient(settings.bot.opts);
 
-Universal.start();
-Universal.deploySlashCommands(true);
-// Universal.server();
+(async () => {
+      Universal.menu();
+      await wait(5000);
+      Universal.handleCommands(join(__dirname, './commands'));
+      Universal.handleEvents(join(__dirname, './events'));
+      Universal.start();
+      Universal.deploySlashCommands(true);
+})();
