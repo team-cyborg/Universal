0fc395a9cc59864c192ee8e58711dc6190d30d2a
team-universal <devinsane.79@gmail.com>
2022-09-28 10:45:05 +0530

    Universal: minor bug fix

--- tests/db.js
+++ tests/db.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-console */
 const { Database } = require('../src/classes/Database');
 
-const db = new Database('finalDB.json');
+const db = new Database('test_db.json');
 
-db.set('name', 'samarth');
-db.set('info', { age: 18, hobby: 'coding' });
-db.set('arr', []);
+db.set('db', { dbname: 'name', dbid: 'dbid', dbuserarr: [] });
+
+(async () => {
+      const x = await db.get('db');
+      console.log(x.dbarr);
+      console.log(await db.push('db.dbarr', 'x'));
+})();
--- tests/logger.js
+++ tests/logger.js
@@ -4,12 +4,12 @@ const { combine, timestamp, label, prettyPrint } = winston.format;
 
 const logger = winston.createLogger({
       transports: [new winston.transports.File({ filename: 'test.log' }), new winston.transports.Console()],
-      format: combine(label({ label: 'right meow!' }),timestamp(),prettyPrint())
+      format: combine(label({ label: 'right meow!' }), timestamp(), prettyPrint())
 });
 
 logger.log({
       level: 'info',
       message: 'A test log',
-})
+});
 
-logger.info('A test log')
+logger.info('A test log');
