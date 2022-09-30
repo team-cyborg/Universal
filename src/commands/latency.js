6a69dac33eb9553bfb3602e5dc10322d73a759e8
team-universal <devinsane.79@gmail.com>
2022-09-30 10:32:30 +0530

    Universal: prettier set as default formatter

--- src/commands/latency.js
+++ src/commands/latency.js
@@ -1,32 +1,34 @@
 module.exports = {
-      data: {
-            name: 'latency',
-            description: 'A latency command.'
-      },
+	data: {
+		name: 'latency',
+		description: 'A latency command.',
+	},
 
-      /**
-       * 
-       * @param {import('../classes/Client').UniClient} universal 
-       * @param {import('discord.js').CommandInteraction} interaction
-       */
-      run: async (universal, interaction) => {
-            const latency = Date.now() - interaction.createdTimestamp;
+	/**
+	 *
+	 * @param {import('../classes/Client').UniClient} universal
+	 * @param {import('discord.js').CommandInteraction} interaction
+	 */
+	run: async (universal, interaction) => {
+		const latency = Date.now() - interaction.createdTimestamp;
 
-            try {
-                  await interaction.reply({
-                        embeds: [
-                              universal.utilities.createBasicEmbed({
-                                    title: universal.settings.bot.embed.title,
-                                    desc: `> My latency is: ${latency}ms!`,
-                                    url: universal.settings.bot.inv_url,
-                                    color: universal.settings.bot.embed.color.default,
-                                    thumbnail: universal.user.displayAvatarURL(),
-                              })
-                        ],
-                        ephemeral: true
-                  });
-            } catch (e) {
-                  universal.loggger.error(e);
-            }
-      }
-}; 
+		try {
+			await interaction.reply({
+				embeds: [
+					universal.utilities.createBasicEmbed({
+						title: universal.settings.bot.embed.title,
+						desc: `> My latency is: ${latency}ms!`,
+						url: universal.settings.bot.inv_url,
+						color: universal.settings.bot.embed.color
+							.default,
+						thumbnail:
+							universal.user.displayAvatarURL(),
+					}),
+				],
+				ephemeral: true,
+			});
+		} catch (e) {
+			universal.loggger.error(e);
+		}
+	},
+};
--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,25 +1,25 @@
 module.exports = {
-      data: {
-            name: 'ping',
-            description: 'A ping pong command.'
-      },
+	data: {
+		name: 'ping',
+		description: 'A ping pong command.',
+	},
 
-      /**
-       * 
-       * @param {import('../classes/Client').UniClient} universal 
-       * @param {import('discord.js').CommandInteraction} interaction
-       */
-      run: async (universal, interaction) => {
-            await interaction.reply({
-                  embeds: [
-                        universal.utilities.createBasicEmbed({
-                              title: universal.settings.bot.embed.title,
-                              desc: `> My ping is: ${universal.ws.ping}ms!`,
-                              url: universal.settings.bot.inv_url,
-                              color: universal.settings.bot.embed.color.default,
-                              thumbnail: universal.user.displayAvatarURL(),
-                        })
-                  ]
-            });
-      }
+	/**
+	 *
+	 * @param {import('../classes/Client').UniClient} universal
+	 * @param {import('discord.js').CommandInteraction} interaction
+	 */
+	run: async (universal, interaction) => {
+		await interaction.reply({
+			embeds: [
+				universal.utilities.createBasicEmbed({
+					title: universal.settings.bot.embed.title,
+					desc: `> My ping is: ${universal.ws.ping}ms!`,
+					url: universal.settings.bot.inv_url,
+					color: universal.settings.bot.embed.color.default,
+					thumbnail: universal.user.displayAvatarURL(),
+				}),
+			],
+		});
+	},
 };
